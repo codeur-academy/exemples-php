a259602175de3cd7682be2e49e29feb83f034d50
ESSARRAJ Fouad <fouad.systems@gmail.com>
2021-05-05 08:54:42 +0000

    connect to mysql

---
+++ prototype1.php
@@ -0,0 +1,15 @@
+<?php
+   
+   // Ouverture de la connexion à la base de données
+   $dbhost = 'localhost:3036';
+   $dbuser = 'guest';
+   $dbpass = 'guest123';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   echo 'Connected successfully';
+   mysql_close($conn);
+?>
